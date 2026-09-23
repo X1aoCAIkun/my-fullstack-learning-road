4a9d79ae4507d54d41b0c880f7e659280bd12216
laoshi <sc20060417@qq.com>
2026-09-23 23:50:37 +0800

    Python学习第一周第一天

---
+++ PythonLearning/Week-1/六大基本数据类型.py
@@ -0,0 +1,9 @@
+a = 43      # a为int（整型），不带小数点的数
+b = 4.3     # b为float（浮点数），带小数点的数
+c = "endeavor"      # c为str（字符串），一段字符
+d = True        # d为bool（布尔型），表示真假，首字要大写
+e = None        # NoneType（无型），“什么都没有”的占位符
+f = b"abc"      # byte（字节型），存储再计算机底层的数据
+
+#type(x),可以运用这个内置函数判断x变量的类型
+print(type(a),type(b),type(c),type(d),type(e),type(f))
